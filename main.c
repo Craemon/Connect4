3d354bdc1b46f77e17723222ed5ee441c07df706
Craemon <121398546+Craemon@users.noreply.github.com>
2025-01-10 14:40:23 +0100

    Basic Menu Implementation

--- main.c
+++ main.c
@@ -1,26 +1,33 @@
 #include <stdio.h>
+#include "game.h"
 
-void printmainmenu() {
-    printf("...Dialoge This is the mainmenu:\n");
+void printintro() {
+    printf("...Dialoge This is connect 4:\n");
+}
+void mainmenu() {
+    char mainmenuoption;
     printf("What do you want to do?\n");
     printf("Type [1] to\n");
     printf("Type [2] to\n");
     printf("Type [q] to quit the game :(\n");
-}
-int main(void) {
-    char mainmenuoption;
-    printmainmenu();
-    scanf("%c", &mainmenuoption);
+    scanf(" %c", &mainmenuoption);
     switch (mainmenuoption) {
-        //Option to quit the game
+        //Enter gameloop
         case '1':
-
+            char board[row][col];
+            gameloop(board);
+        //Option to quit the game
         case 'q':
             printf("Thank you for playing Connect4!\n");
-        return 0;
-        //Detect invalid inputs
+            return;
+        //Detect invalid inputs(try again)
         default:
             printf("This is not a valid input! Please try again!\n");
+            mainmenu();
     }
+}
+int main(void) {
+    printintro();
+    mainmenu();
     return 0;
 }
