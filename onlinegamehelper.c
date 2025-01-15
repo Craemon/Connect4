786502a0bf175541261b660761d260d6d5528a7d
Craemon <121398546+Craemon@users.noreply.github.com>
2025-01-15 13:50:08 +0100

    gamecode gen comments

--- onlinegamehelper.c
+++ onlinegamehelper.c
@@ -2,12 +2,12 @@
 #include <string.h>
 
 void gengamecode(int row, int col, char board[row][col], int currentplayer) {
+    //first converts 2d-array to a 1d-array (because I'm too stupid to do it without extra steps)
     char onedarray[42];
     int onedarrayindex = 0;
     for(int j = 0; j < sizeof onedarray; j++) {
         onedarray[j] = ' ';
     }
-
     for (int i = 0; i < row; i++) {
         for (int j = 0; j < col; j++) {
             char convertspace = board[i][j];
@@ -19,18 +19,18 @@ void gengamecode(int row, int col, char board[row][col], int currentplayer) {
         }
     }
 
-    // Null-terminate the string after filling the array
-    onedarray[onedarrayindex] = '\0';
-
+    //init gamecode array
     char currentchar='€'; int consecutivechars = 0; //initialize with impossible character "€"
     char gamecode[86]; int gamecodeindex = 0;
     for(int i = 0; i < strlen(onedarray); i++) {
         gamecode[i] = ' ';
     }
+    //first number in gamecode indicates whose turn it currently is
     char currentplayerchar[1];
     sprintf(currentplayerchar, "%d", currentplayer);
     gamecode[gamecodeindex] = currentplayerchar[0];
     gamecodeindex++;
+    //compresses the created 1d-array to the final gamecode
     for (int i = 0; i <= strlen(onedarray); i++) {
         if (onedarray[i] == currentchar) {
             consecutivechars++;
@@ -62,9 +62,7 @@ void gengamecode(int row, int col, char board[row][col], int currentplayer) {
             gamecodeindex++;
         }
     }
-
-    // Null-terminate the encoded string
-    gamecode[gamecodeindex] = '\0';
+    //output final gamecode
     printf("Send this gamecode to another person!\n");
     printf("They can enter it by choosing option 3 in the main menu:\n");
     printf("%s", gamecode);
